1ee5ecded86e9efefb8dd28e5f8232cb4c4c0099
Ben Wong <ben@anzac.local>
2014-03-21 16:11:54 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ScraperH::Application.config.secret_key_base = 'ea9277f91e311b90efe31c6c856cafd92a3234e416dd398ffcbee89e621cf09b63976c1c82f8c85efae1730c70316ed585ba6ec27c496f36f62474359d86757c'
